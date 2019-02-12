d59f0c0b779993bf6145e904a633d595c17279d5
Shivva <ssrinath@corp.untd.com>
2019-02-12 17:10:54 +0530

    First Git Commit for Second Maven Project

---
+++ src/main/java/com/qa/tests/DemoTest1.java
@@ -0,0 +1,47 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+
+public class DemoTest1 {
+	
+	@Test
+	public void sum()
+	{		
+		System.out.println("Addition");
+		int a=50;
+		int b=30;
+		Assert.assertEquals(80, a+b);		
+	}
+	
+	@Test
+	public void substraction()
+	{		
+		System.out.println("Substraction");
+		int a=50;
+		int b=70;
+		Assert.assertEquals(20, b-a);		
+	}
+	
+	@Test
+	public void div()
+	{	
+		System.out.println("Division");
+		int a=80;
+		int b=20;
+		Assert.assertEquals(4, a/b);		
+	}
+	
+	@Test
+	public void multi()
+	{		
+		System.out.println("Multiplication");
+		int a=5;
+		int b=10;
+		Assert.assertEquals(50, a*b);		
+	}
+	
+	
+
+}
